475fb284a46fa4a9682e05220b8e9777a453ed4e
Guilherme Santana <87387380+guisduarte@users.noreply.github.com>
2023-01-16 12:18:06 -0300

    Atualizei o JavaScript

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-//Sincrono = uma linha por vez na ordem que for escrita.
+let consultaCEP = fetch ('https://viacep.com.br/ws/01001000/json/');
 
-console.log("Mandando oi pro amigo!");
-
-function mandaMensagem() {
-    console.log("Tudo bem?");
-    console.log("Vou te mandar uma solicitação!");
-    console.log("Solicitação recebida!");
-}
-
-mandaMensagem();
-
-console.log("Tchau tchau!"); 
-
-// Assincrono = setTimeOut => faz esperar um tempo determinado para a função ser chamada, nesse caso 5 segundos (5 mil milesegundos). Lê a próxima escrita, e depois do 5 segundos, executa a função que ficou aguardando.
-
-console.log("Mandando oi pro amigo!");
-
-function mandaMensagem() {
-    console.log("Tudo bem?");
-    console.log("Vou te mandar uma solicitação!");
-    console.log("Solicitação recebida!");
-}
-
-setTimeout(mandaMensagem, 5000);
-
-console.log("Tchau tchau!");
+console.log(consultaCEP);
