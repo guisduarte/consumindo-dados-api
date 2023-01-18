c8cf33e077d48f109ac61ad10da9acc593a6b5ac
Guilherme Santana <87387380+guisduarte@users.noreply.github.com>
2023-01-18 13:26:30 -0300

    atualizei javaScript

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-let consultaCEP = fetch ('https://viacep.com.br/ws/01001000/json/')
-.then(resposta => resposta.json())
-.then(r => {
-    if (r.erro) {
-        throw Error('Esse cep não existe!')
-    } else
-    console.log(r)
-})
-.catch(erro => console.log(erro))
-.finally(mensagem => console.log('Processamento concluído!'));
+async function buscaEndereco(cep) {
+    try {
+        let consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        let consultaCEPCovertida = await consultaCEP.json();
+        if(consultaCEPCovertida.erro) {
+            throw Error ('CEP não existente!')
+        }
+        console.log(consultaCEPCovertida);
+        return consultaCEPCovertida;
+    } catch (erro) {
+        console.log(erro)
+    }
+}
 
-console.log(consultaCEP);
